e13cd651b58fc7a6126d21e14eddfa1119e1f38b
Diego Rodrigues <ddiegosr@hotmail.com>
2018-06-23 18:13:39 -0300

    Classe de View(Template Engine) incrementada. Adicionada sintaxe @extends, @section

---
+++ core/Tokenizer.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: diego
+ * Date: 16/06/18
+ * Time: 22:11
+ */
+
+namespace Core;
+
+
+class Tokenizer
+{
+    protected $patterns = [];
+    protected $sectionRule = [];
+    protected $layoutRule;
+    protected $constructRule;
+
+    protected function addRule(string $pattern, string $replacement)
+    {
+        $this->patterns[] = [$pattern => $replacement];
+    }
+
+    protected function setLayoutRule(string $layoutRule)
+    {
+        $this->layoutRule = $layoutRule;
+    }
+
+    protected function getLayoutRule()
+    {
+        return $this->layoutRule;
+    }
+
+    protected function setSectionRule(string $openingTag, string $closingTag)
+    {
+        $this->sectionRule['openingTag'] = $openingTag;
+        $this->sectionRule['closingTag'] = $closingTag;
+    }
+
+    protected function getSectionRuleByKey(string $key)
+    {
+        return $this->sectionRule[$key];
+    }
+
+    protected function setConstructRule($constructRule)
+    {
+        $this->constructRule = $constructRule;
+    }
+
+    protected function getConstructRule()
+    {
+        return $this->constructRule;
+    }
+}
--- core/View.php
+++ core/View.php
@@ -3,9 +3,10 @@
 namespace Core;
 
 
-class View
+class View extends Tokenizer
 {
-    private $patterns = [];
+    private $layout;
+    private $blocks = [];
 
     public function __construct()
     {
@@ -19,44 +20,122 @@ class View
         $this->addRule('/@foreach ?\((.*) as (.*)( ?=> ?.*)?\)/', '<?php foreach($1 as $2 $3): ?>');
         $this->addRule('/@while ?\((.*)\)/', '<?php while($1): ?>');
         $this->addRule('/@end(if|foreach|for|while)/', '<?php end$1 ?>');
+
+        $this->setLayoutRule("/@extends ?\('(.*)'\)/");
+        $this->setSectionRule("/@section ?\('(.*)'\)/", '/@endsection/');
+        $this->setConstructRule("/@yield ?\('(.*)'\)/");
     }
 
-    private function addRule(string $pattern, string $replacement){
-        $this->patterns[] = [$pattern => $replacement];
+    public function getLayout(): string
+    {
+        return $this->layout;
     }
 
-    public function addFunction(string $function, array $params = []){
-        if(function_exists($function)){
-            if(empty($params)){
-                $this->addRule("/@{$function}\(\)/", "<?php {$function}() ?>");
-            } else {
-                $this->addRule("/@{$function}\((.*)\)/", "<?php {$function}($1) ?>");
+    public function setLayout(string $view): void
+    {
+        $file = fopen($view, 'r');
+
+        while (!feof($file)) {
+            $line = fgets($file);
+
+            if (preg_match($this->getLayoutRule(), $line, $matches)) {
+                $this->layout = __DIR__ . "/../app/views/layout/{$matches[1]}.phtml";
+                break;
             }
         }
+
+        fclose($file);
     }
 
-    private function loadTemplate(string $template)
+    public function getBlocks(): array
     {
-        $templatePath = __DIR__ . "/../app/views/{$template}.phtml";
-        if(file_exists($templatePath)){
-            return file_get_contents($templatePath);
-        } else {
-            return false;
+        return $this->blocks;
+    }
+
+    public function compile(string $view): string
+    {
+        $tempView = $view;
+
+        foreach ($this->patterns as $pattern) {
+            $tempView = preg_replace(array_keys($pattern), array_values($pattern), $tempView);
         }
+
+        return $tempView;
     }
 
-    public function render(string $template, array $data = [], string $layout = "")
+    public function setBlocks(string $view): void
     {
-        $contents = $this->loadTemplate($template);
+        $file = fopen($view, 'r');
 
-        if(!empty($data)){
+        $sectionName = '';
+        $contentBlocks = [];
+        while (!feof($file)) {
+            $line = fgets($file);
+
+            if (preg_match($this->getSectionRuleByKey('openingTag'), $line, $matches)) {
+                $sectionName = $matches[1];
+            }
+
+            $contentBlocks[$sectionName] .= $line;
+            
+            if (preg_match($this->getSectionRuleByKey('closingTag'), $line, $matches)) {
+                $sectionName = '';
+            }
+        }
+
+        $contentBlocks = array_filter($contentBlocks, function ($key) {
+            return !empty($key);
+        }, ARRAY_FILTER_USE_KEY);
+
+        $newBlocks = [];
+        foreach ($contentBlocks as $blockName => $block) {
+            preg_match("/@section\('(.*)'\)((.|\n)*)@endsection/", $block, $matches);
+            $newBlocks[$blockName] = trim($matches[2]);
+        }
+
+        fclose($file);
+        $this->blocks = $newBlocks;
+    }
+
+    public function compileLayout(string $layoutName): string
+    {
+        $file = fopen($layoutName, 'r');
+        $result = '';
+        while (!feof($file)) {
+            $line = fgets($file);
+
+            preg_match($this->getConstructRule(), $line, $matches);
+            if (array_key_exists($matches[1], $this->getBlocks())) {
+                $line = preg_replace($this->getConstructRule(), $this->getBlocks()[$matches[1]], $line);
+            } else {
+                $line = preg_replace($this->getConstructRule(), "", $line);
+            }
+
+            $result .= $line;
+        }
+        fclose($file);
+        return $result;
+    }
+
+    public function render(string $view, array $data = []): void
+    {
+
+        if (!empty($data)) {
             extract($data);
         }
 
-        foreach ($this->patterns as $pattern) {
-            $contents = preg_replace(array_keys($pattern), array_values($pattern), $contents);
+        $view = __DIR__ . "/../app/views/{$view}.phtml";
+        $this->setLayout($view);
+
+        if ($this->layout != null) {
+            $this->setBlocks($view);
+            $content = $this->compile($this->compileLayout($this->getLayout()));
+        } else {
+            $viewContent = file_get_contents($view);
+            $content = $this->compile($viewContent);
         }
 
-        eval(' ?>' . $contents . '<?php ');
+        eval(' ?> ' . $content . '<?php ');
     }
+
 }
