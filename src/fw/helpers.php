cbf49e1fffa8187522d2d2d4e20da763f94f54de
Diego Rodrigues <ddiegosr@hotmail.com>
2018-07-18 21:00:04 -0300

    Implementado suporte a .env files

--- src/fw/helpers.php
+++ src/fw/helpers.php
@@ -17,3 +17,20 @@ function view_register_function(Callable $functionName): void
     $viewObject = \MyFw\View::getInstance();
     $viewObject->registerFunction($functionName);
 }
+
+function env(string $envName, string $default): string
+{
+    $variables = [];
+    $dotenv = new \Dotenv\Dotenv(__DIR__ . '/../../');
+
+    foreach ($dotenv->load() as $var) {
+        $pieces = explode('=', $var);
+        $variables[$pieces[0]] = $pieces[1];
+    }
+
+    if (array_key_exists($envName, $variables)) {
+        return $variables[$envName];
+    }
+
+    return $default;
+}
